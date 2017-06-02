3de200867c29603e48a44c6cc668405a757b3e5d
Hell0ow <antoine.savalle1@viacesi.fr>
2017-06-02 11:38:18 +0200

    Main update 1

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,16 @@
+import jpu2016.dogfight.controller.*;
+import jpu2016.dogfight.view.*;
+import jpu2016.dogfight.model.*;
+import jpu2016.gameframe.*;
+
 
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
+		final DogfightModel dogfightModel = new DogfightModel(); 
+		final DogfightController dogfightController = new DogfightController(dogfightModel); 
+		final DogfightView dogfightView = new DogfightView(dogfightController, dogfightModel, dogfightModel); 
+		dogfightController.setViewSystem(dogfightView); dogfightController.play(); 
 	}
-
 }
