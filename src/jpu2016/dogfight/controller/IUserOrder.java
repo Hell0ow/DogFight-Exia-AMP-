4a6523ada80fa634f66e41c89327ba4ec3344b5b
Hell0ow <antoine.savalle1@viacesi.fr>
2017-06-02 11:27:35 +0200

    [Controller] Phase 2 Structure

--- src/jpu2016/dogfight/controller/IUserOrder.java
+++ src/jpu2016/dogfight/controller/IUserOrder.java
@@ -1,5 +1,8 @@
 package jpu2016.dogfight.controller;
 
 public interface IUserOrder{
-
+	
+	//GETTERS
+	public Order getOrder();
+	public int getPlayer();
 }
--- src/jpu2016/dogfight/controller/UserOrder.java
+++ src/jpu2016/dogfight/controller/UserOrder.java
@@ -1,5 +1,28 @@
 package jpu2016.dogfight.controller;
 
 public class UserOrder implements IUserOrder {
+	
+	protected int player;
+	protected Order order;
 
+	//CONTRUCTOR
+	public UserOrder(int player, Order order) {
+		this.player = player;
+		this.order = order;
+	}
+
+	
+	//GETTERS
+	public int getPlayer() {
+		return player;
+	}
+	public Order getOrder(){
+		return order;
+	}
+	
+	
+
+	
+	
+	
 }
