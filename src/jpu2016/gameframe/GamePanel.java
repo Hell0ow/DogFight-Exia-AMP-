0c8a3388be08377f9211c0326e3b786f13c8e364
PierrotHml <pierre.hamel@viacesi.fr>
2017-06-02 16:16:58 +0200

    modif GameFrame et Panel et GraphicsBuilder

--- src/jpu2016/gameframe/GamePanel.java
+++ src/jpu2016/gameframe/GamePanel.java
@@ -6,7 +6,12 @@ import java.util.Observer;
 
 import javax.swing.JPanel;
 
+import jpu2016.dogfight.view.GraphicsBuilder;
+
 public class GamePanel extends JPanel implements Observer {
+	
+	private static final long serialVersionUID = 3987064896591403626L;
+	private final GraphicsBuilder graphicsBuilder;
 
 	public GamePanel(IGraphicsBuilder graphicBuilder) {
 		
@@ -17,7 +22,7 @@ public class GamePanel extends JPanel implements Observer {
 	}
 	
 	public void paintComponent(Graphics graphic) {
-		
+		this.graphicsBuilder.applyModelToGraphic(graphic, observer);
 	}
 
 	@Override
